4d407e07b9815d34e37ff8d33c8dbfa2d685fbfe
PedroNuernberg <pedrobnuernberg@gmail.com>
2023-06-04 00:01:38 -0300

    interface terceiros

---
+++ src/app/terceiros/models/Terceiro.ts
@@ -0,0 +1,15 @@
+export interface Terceiro {
+    id: string;
+    razaoSocial: string;
+    cnpjTerceiro: string;
+    enderecoTerceiro: string;
+    cepTerceiro: string;
+    bairroTerceiro: string;
+    numeroTerceiro: string;
+    enumStatus: string;
+    telefoneTerceiro: string;
+    contatoTerceiro: string;
+
+    //Usuario
+  
+}
