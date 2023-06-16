e46cd633f2705bd2e036bc7110dafa8bb3bc11b5
PedroNuernberg <pedrobnuernberg@gmail.com>
2023-06-16 18:24:36 -0300

    iniciar tela de login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -5,16 +5,20 @@ import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { SharedModule } from '../shared/shared.module';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
     LoginRoutingModule,
     AppMaterialModule,
-    SharedModule
-  ]
+    SharedModule,
+    MatButtonModule,
+    MatCardModule
+  ],
 })
-export class LoginModule { }
+export class LoginModule {}
--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,10 +1,53 @@
+import { LoginService } from './../services/login.service';
 import { Component } from '@angular/core';
+import { NonNullableFormBuilder } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
+
 export class LoginComponent {
 
+  form = this.formBuilder.group({
+    email: [''],
+    senha: ['']
+  });
+
+  constructor(private formBuilder: NonNullableFormBuilder,
+    private route: ActivatedRoute,
+    private service: LoginService,
+    private snackBar: MatSnackBar) {
+  }
+
+  // ngOnInit(): void {
+
+  //   this.LoginService.GetLogin()
+  //     .subscribe(dados => this.login = dados);
+
+  //   const login: Login = this.route.snapshot.data['login'];
+  //   this.form.setValue({
+  //     email: login.email,
+  //     senha: login.senha,
+  //   })
+  // }
+
+  // onSubmit() {
+  //   this.service.save(this.form.value)
+  //   .subscribe(result => this.onSuccess(), error => this.onError());
+  // }
+
+  // private onSuccess() {
+  //   this.snackBar.open('Login realizado!', '', {duration: 3000 });
+  // }
+
+  // private onError() {
+  //   this.snackBar.open('Ops, tente novamente!', '', {duration: 3000 });
+  // }
 }
+
+
+
