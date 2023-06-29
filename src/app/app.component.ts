9d032e666afa691a62a6c76fab421d2e16398320
PedroNuernberg <pedrobnuernberg@gmail.com>
2023-06-29 00:35:13 -0300

    mostrar nome do username nas toolbars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'fronendTCS';
 
+  nomeUsuario: string | null = localStorage.getItem('user_nome');
+
   mostrarMenu: boolean = false;
 
   // constructor(private login: LoginService) {
